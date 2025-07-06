2540c05e6af9a4f7259f877c1ecbed69b6fd5da7
Nimrod Zimerman <zimerman@fastmail.fm>
2025-07-06 23:46:28 +0300

    Support loading and saving binary secrets

--- src/js/app.js
+++ src/js/app.js
@@ -11,13 +11,22 @@
     DOM.generated = $(".generated");
     DOM.parts = $(".parts");
     DOM.combined = $(".combined");
+    DOM.combinedSize = $(".combined-size");
+    DOM.secretSize = $(".secret-size");
+    DOM.loadBinary = $(".btn-load-binary");
+    DOM.saveBinary = $(".btn-save-binary");
+
+    var secretBytes = new Uint8Array(0);
+    var combinedBytes = new Uint8Array(0);
 
     function init() {
         // Events
         DOM.required.addEventListener("input", generateParts);
         DOM.total.addEventListener("input", generateParts);
-        DOM.secret.addEventListener("input", generateParts);
+        DOM.secret.addEventListener("input", generatePartsFromTextArea);
         DOM.parts.addEventListener("input", combineParts);
+        DOM.loadBinary.addEventListener("click", loadBinary);
+        DOM.saveBinary.addEventListener("click", saveBinary);
     }
 
     // Convert a UTF16 string to UTF8 bytes.
@@ -32,14 +41,33 @@
         return decoder.decode(UTF8Bytes);
     }
 
+    // Generate shares from an input in the text area
+    function generatePartsFromTextArea() {
+        var secret = DOM.secret.value;
+        secretBytes = UTF16StringToUTF8Bytes(secret);
+
+        generateParts();
+    }
+
+    // Generate shares from an input in a file
+    function generatePartsFromBytes(bytes) {
+        secretBytes = bytes;
+        DOM.secret.value = UTF8BytesToUTF16String(secretBytes);
+
+        generateParts();
+    }
+
+    // Generate shares from secret bytes loaded into secretBytes
     function generateParts() {
         // Clear old generated
         DOM.generated.innerHTML = "";
+        // Update the secret size
+        DOM.secretSize.textContent = "(" + secretBytes.length + " bytes)";
+
         // Get the input values
-        var secret = DOM.secret.value;
-        var secretBytes = UTF16StringToUTF8Bytes(secret);
         var total = parseFloat(DOM.total.value);
         var required = parseFloat(DOM.required.value);
+
         // validate the input
         if (total < 2) {
             DOM.error.textContent = "Total must be at least 1";
@@ -69,7 +97,7 @@
             DOM.error.textContent = "Required must be less than total";
             return;
         }
-        else if (secret.length == 0) {
+        else if (secretBytes.length == 0) {
             DOM.error.textContent = "Secret is blank";
             return;
         }
@@ -100,14 +128,51 @@
         var parts = partsStr.trim().split(/\s+/);
         // Combine the parts
         try {
-            var secretBytes = secrets.combine(parts);
-            var combined = UTF8BytesToUTF16String(secretBytes);
+            combinedBytes = secrets.combine(parts);
+            var combined = UTF8BytesToUTF16String(combinedBytes);
         }
         catch (e) {
             DOM.combined.textContent = e.message;
         }
         // Display the combined parts
         DOM.combined.textContent = combined;
+        DOM.combinedSize.textContent = "(" + combinedBytes.length + " bytes)";
+    }
+
+    // Load a binary file as the secret
+    function loadBinary() {
+        var input = document.createElement('input');
+        input.type = 'file';
+
+        input.onchange = function(event) {
+            var file = event.target.files[0];
+            if (file) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    var loadedBytes = new Uint8Array(e.target.result);
+                    generatePartsFromBytes(loadedBytes);
+                };
+
+                reader.readAsArrayBuffer(file);
+            }
+        };
+        input.click();
+    }
+
+    // Save the combined secret as a binary file
+    function saveBinary() {
+        var blob = new Blob([combinedBytes], { type: 'application/octet-stream' });
+        var url = URL.createObjectURL(blob);
+
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = "secret.dat";
+        document.body.appendChild(a);  // Needed to work-around behavior in old Firefox versions
+        a.click();
+        document.body.removeChild(a);
+
+        // Should revoke the object URL to free up memory, but this may cause a race condition
+        //URL.revokeObjectURL(url);
     }
 
     init();
